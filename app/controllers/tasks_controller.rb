95e9a79ac276eb65ad2ae1bc2bcac5d4c3f12152
yoichi ogo <o541.1980@gmail.com>
2021-08-25 12:39:49 +0900

    taskの新規投稿機能を追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,21 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to tasks_path, notice: "新規タスクを作成しました"
+    else
+      render :new
+    end
+  end
+  private
+  def task_params
+    params.require(:task).permit(:task_name, :detail)
+  end
 end
