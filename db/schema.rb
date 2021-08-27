7b899188e0f5453e73e3d243f205f9b3dd0d207f
yoichi ogo <o541.1980@gmail.com>
2021-08-28 00:41:17 +0900

    終了期限カラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_25_010037) do
+ActiveRecord::Schema.define(version: 2021_08_27_142440) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2021_08_25_010037) do
     t.string "detail", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "deadline", default: "3000-01-01 00:00:00", null: false
   end
 
 end
