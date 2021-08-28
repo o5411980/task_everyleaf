fadaddca230cdca9a49dd7aff003cfd6e7a4a662
yoichi ogo <o541.1980@gmail.com>
2021-08-28 22:38:07 +0900

    ステータス機能を追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -45,6 +45,6 @@ class TasksController < ApplicationController
 
   private
   def task_params
-    params.require(:task).permit(:task_name, :detail, :deadline)
+    params.require(:task).permit(:task_name, :detail, :deadline, :status)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_27_142440) do
+ActiveRecord::Schema.define(version: 2021_08_28_120711) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2021_08_27_142440) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "deadline", default: "3000-01-01 00:00:00", null: false
+    t.integer "status"
   end
 
 end
