2b07deb170568a658952a938892afe8910402423
yoichi ogo <o541.1980@gmail.com>
2021-08-30 22:02:56 +0900

    CRUD機能に優先順位を追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -55,6 +55,6 @@ class TasksController < ApplicationController
 
   private
   def task_params
-    params.require(:task).permit(:task_name, :detail, :deadline, :status)
+    params.require(:task).permit(:task_name, :detail, :deadline, :status, :priority)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_29_095639) do
+ActiveRecord::Schema.define(version: 2021_08_30_101250) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2021_08_29_095639) do
     t.datetime "updated_at", null: false
     t.datetime "deadline", default: "3000-01-01 00:00:00", null: false
     t.integer "status"
+    t.integer "priority", default: 0, null: false
     t.index ["id"], name: "index_tasks_on_id"
   end
 
