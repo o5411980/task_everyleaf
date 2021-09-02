d5058ac51a5920727eea7952e18031f0ced18bb2
yoichi ogo <o541.1980@gmail.com>
2021-09-02 11:28:36 +0900

    ログイン有無による画面遷移を設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,29 @@
 class UsersController < ApplicationController
+  skip_before_action :login_required, only: [:new, :create]
   def new
-    @user = User.new
+    unless current_user
+      @user = User.new
+    else
+      redirect_to tasks_path
+    end
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to user_path(@user.id), notice: "your account created"
+      session[:user_id] = @user.id
+      redirect_to user_path(@user.id), notice: "アカウントが作成できました"
     else
       render :new
     end
   end
 
   def show
-    @user = User.find(params[:id])
+    if params[:id] == (current_user.id.to_s)
+      @user = User.find(params[:id])
+    else
+      redirect_to tasks_path
+    end
   end
 
   private
